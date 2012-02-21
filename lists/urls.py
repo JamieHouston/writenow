16d16db317492356bb8316db433bee3c71bcc5ee
foresterh <foresterh@gmail.com>
2012-02-21 15:38:14 -0800

    removed accounts (for now)

--- lists/urls.py
+++ lists/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('lists.views',
-    url(r'^$', 'user_lists'),
+    url(r'^$', 'home'),
     url(r'^(?P<user_name>[\w\s\d]+)/$', 'user_lists'),
     url(r'^(?P<user_name>[\w\s\d]+)/(?P<list_name>[\w\s\d]+)/$', 'view_list'),
     url(r'^(?P<user_name>[\w\s\d]+)/(?P<list_name>[\w\s\d]+)/add/(?P<new_item>[\w\s\d]+)$', 'add_item'),
--- settings.py
+++ settings.py
@@ -111,11 +111,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    #'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'lists',
-    'accounts',
+    #'accounts',
 )
 
 # A sample logging configuration. The only tangible logging
