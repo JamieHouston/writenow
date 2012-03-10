58cdf165415caad767947f174de517dd5d852f18
foresterh <foresterh@gmail.com>
2012-03-10 07:41:10 -0800

    added mobile detection

--- settings.py
+++ settings.py
@@ -95,6 +95,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'minidetector.Middleware',
 )
 
 ROOT_URLCONF = 'writenow.urls'
